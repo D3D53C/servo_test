dfd4624aa9b98bd9fcff45fd2089c3b583e03385
D3D53C <marc.steinebrunner@gmail.com>
2017-07-15 00:11:32 +0200

    Update test05.py

--- test05.py
+++ test05.py
@@ -6,15 +6,10 @@ GPIO.setup(7, GPIO.OUT)
 
 P = GPIO.PWM(7, 50)
 P.start(7.5)
-try:
-    while True:
+while True:
         P.ChangeDutyCicle(0.5)
         time.sleep(2)
         P.ChangeDutyCicle(1.5)
         time.sleep(2)
         P.ChangeDutyCicle(2.5)
         time.sleep(3)
-except:
-
-    print ("Stop")
-    pass
