9063dbeb71586e3c4696ebc05e606e749e5d4107
D3D53C <marc.steinebrunner@gmail.com>
2017-07-14 18:09:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ from servo import Servo as Servo
 import time
 
 class Main:
-    def main(self):
+    def __init__(self):
         self.servo = Servo(3,50,500,2500,180)
+        main()
+    def main(self):
         while True:
-            self.servo.change_position(0)
+            servo.change_position(0)
             print("0 Degrees")
             time.sleep(2)
-            self.servo.change_position(90)
+            servo.change_position(90)
             print("90 Degrees")
             time.sleep(2)
-            self.servo.change_position(180)
+            servo.change_position(180)
             print ("180 Degrees")
             time.sleep(5)
-
-    main()
