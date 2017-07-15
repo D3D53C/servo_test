61df5acd294de62dfeae948ff95c7ebe433521ad
D3D53C <marc.steinebrunner@gmail.com>
2017-07-15 18:26:07 +0200

    Update test05.py

--- test05.py
+++ test05.py
@@ -2,9 +2,9 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+GPIO.setup(3, GPIO.OUT)
 
-P = GPIO.PWM(7, 50)
+P = GPIO.PWM(3, 50)
 P.start(7.5)
 while True:
         P.ChangeDutyCycle(0.5)
