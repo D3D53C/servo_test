b0b77a80231ae6eb2b39dba51f5e4e330e78e086
D3D53C <marc.steinebrunner@gmail.com>
2017-07-15 16:35:19 +0200

    Add files via upload

--- test06.py
+++ test06.py
@@ -1,12 +1,47 @@
-from servo import Servo as Servo
+import RPi.GPIO as GPIO
 import time
 
-servo = Servo(7,50,2500,12500,180)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(3, GPIO.OUT)
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(7, GPIO.OUT)
+GPIO.setup(11, GPIO.OUT)
+GPIO.setup(13, GPIO.OUT)
 
-while True:
-    servo.change_position(180)
-    time.sleep(2)
-    servo.change_position(90)
-    time.sleep(2)
-    servo.change_position(0)
-    time.sleep(2)
+P1 = GPIO.PWM(3, 50)
+P2 = GPIO.PWM(5, 50)
+P3 = GPIO.PWM(7, 50)
+P4 = GPIO.PWM(11, 50)
+P5 = GPIO.PWM(13, 50)
+
+P1.start(7.5)
+P2.start(7.5)
+P3.start(7.5)
+P4.start(7.5)
+P5.start(7.5)
+try:
+    while True:
+        P1.ChangeDutyCicle(0.5)
+        P2.ChangeDutyCicle(0.5)
+        P3.ChangeDutyCicle(0.5)
+        P4.ChangeDutyCicle(0.5)
+        P5.ChangeDutyCicle(0.5)
+
+        time.sleep(2)
+        P1.ChangeDutyCicle(1.5)
+        P2.ChangeDutyCicle(1.5)
+        P3.ChangeDutyCicle(1.5)
+        P4.ChangeDutyCicle(1.5)
+        P5.ChangeDutyCicle(1.5)
+
+        time.sleep(2)
+        P1.ChangeDutyCicle(2.5)
+        P2.ChangeDutyCicle(2.5)
+        P3.ChangeDutyCicle(2.5)
+        P4.ChangeDutyCicle(2.5)
+        P5.ChangeDutyCicle(2.5)
+        time.sleep(3)
+except:
+
+    print ("Stop")
+    pass
